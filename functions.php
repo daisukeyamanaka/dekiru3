e346b5ad1ad1ac6387300fe9173b237b7adefd4c
daisukeyamanaka <87561867+daisukeyamanaka@users.noreply.github.com>
2021-10-07 23:25:53 +0900

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+
+function connect_to_db()
+{
+  $dbn = 'mysql:dbname=gsacfs_d03_11;charset=utf8;port=3306;host=localhost';
+  $user = 'root';
+  $pwd = '';
+
+  try {
+    return new PDO($dbn, $user, $pwd);
+  } catch (PDOException $e) {
+    echo json_encode(["db error" => "{$e->getMessage()}"]);
+    exit();
+  }
+}
+
+function check_session_id()
+{
+  if (!isset($_SESSION["session_id"]) ||$_SESSION["session_id"] != session_id()) {
+    header('Location:todo_login.php');
+    exit();
+  } else {
+    session_regenerate_id(true);
+    $_SESSION["session_id"] = session_id();
+  }
+}
